c0a142b39a47314c49540c54718a71cc7147a1dc
imdev2002 <khainguyendang20@gmail.com>
2022-02-09 23:00:14 +0700

    the first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClass();
+        panel.classList.toggle('active');
+
+    })
+})
+
+function removeActiveClass() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+}
